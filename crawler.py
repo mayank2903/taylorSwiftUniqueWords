8e1669673f6ef292ae9e017132947b37690dc9e4
mayank2903 <pc.mayank@gmail.com>
2023-11-28 23:03:22 +0530

    Added few more checks and fixes. Got results for a few more artists.

--- crawler.py
+++ crawler.py
@@ -10,41 +10,33 @@ from collections import defaultdict
 import csv
 from nltk.corpus import stopwords
 
-URL = "https://www.azlyrics.com/t/taylorswift.html"
+# Note: Change URL and PARENT_DIR for getting results of any new artist.
+URL = "https://www.azlyrics.com/e/edsheeran.html"
 PARENT_URL = "https://www.azlyrics.com"
-PARENT_DIR = "word_frequencies/"
+PARENT_DIR = "word_frequencies/edsheeran/"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36,gzip(gfe)"}
-PROXIES = [
-    'http://114.121.248.251:8080',
-    'http://222.85.190.32:8090',
-    'http://47.107.128.69:888',
-    'http://41.65.146.38:8080',
-    'http://190.63.184.11:8080',
-    'http://45.7.135.34:999',
-    'http://141.94.104.25:8080',
-    'http://222.74.202.229:8080',
-    'http://141.94.106.43:8080',
-    'http://191.101.39.96:80'
-]
 
-
-# todo: make it a parallel crawler. i can parallely fetch all song lyrics.
-# but do this only after metrics are converted into charts well
-# since this does not change the data to be fetched.
+# todo: ed sheeran
 
 
 class LyricsCrawler:
 
     def __init__(self, url):
         self.url = url
-        self.proxy_index = 0
 
     def get_all_song_lyrics_urls(self, all_songs_page_url):
         r = requests.get(all_songs_page_url, headers=HEADERS)
         soup = BeautifulSoup(r.content, 'html5lib')
         song_urls = soup.findAll('div', {'class': 'listalbum-item'})
-        return [self.canonicalize_url(song_url.a['href']) for song_url in song_urls]
+        canonicalized_urls = []
+        for song_url in song_urls:
+            if "google.com" not in song_url.a['href']:
+                canonicalized_url = self.canonicalize_url(song_url.a['href'])
+                canonicalized_urls.append(canonicalized_url)
+            else:
+                print('Ignoring url:', song_url.a['href'])
+        return canonicalized_urls
 
     def canonicalize_url(self, relative_path):
         if relative_path.startswith(PARENT_URL):
@@ -53,11 +45,8 @@ class LyricsCrawler:
             return PARENT_URL + relative_path
 
     def get_word_list_for_song(self, url, stop_words):
-        proxy = PROXIES[self.proxy_index]
-        self.proxy_index = (self.proxy_index + 1) % len(PROXIES)
-        r = requests.get(url=url, headers=HEADERS,
-                         proxies={'http': proxy, 'https': proxy})
-        time.sleep(random.randint(5, 10))
+        r = requests.get(url=url, headers=HEADERS)
+        time.sleep(random.randint(30, 40))
         soup = BeautifulSoup(r.content, 'html.parser')
         # Find section which has song lyrics.
         lyrics = soup.find_all(lambda tag: tag.name == 'div' and not tag.attrs)[0].text
@@ -76,7 +65,6 @@ class LyricsCrawler:
         word_freq = defaultdict(lambda: 0, word_freq)
 
         for word in word_list:
-            # print ('word=', word)
             word_freq[word] += 1
         return word_freq
 
@@ -99,6 +87,13 @@ class LyricsCrawler:
         self.export_dict_to_csv(file_name, word_freq)
 
     def export_dict_to_csv(self, file_name, word_freq):
+        # Sort word frequencies in descending order.
+        word_freq = dict(sorted(word_freq.items(), key=lambda item: item[1], reverse=True))
+
+        # First ensure directory exists
+        if not os.path.exists(PARENT_DIR):
+            os.makedirs(PARENT_DIR)
+
         # Populate the file.
         with open(file_name, 'w') as csv_file:
             writer = csv.writer(csv_file)
@@ -134,7 +129,6 @@ class LyricsCrawler:
 
         # Sorting links by the song names. Helps track how far we are in crawling.
         song_lyrics_page_urls = sorted(song_lyrics_page_urls, reverse=True)
-        print(song_lyrics_page_urls)
 
         # Collect all words of all songs by the artist.
         all_song_words = []
