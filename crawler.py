755fd3d866aa6f18341b9bbda63bb94d5a49bf8a
mayank2903 <pc.mayank@gmail.com>
2023-11-28 23:03:59 +0530

    removed obsolete todo.

--- crawler.py
+++ crawler.py
@@ -17,8 +17,6 @@ PARENT_DIR = "word_frequencies/edsheeran/"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36,gzip(gfe)"}
 
-# todo: ed sheeran
-
 
 class LyricsCrawler:
 
